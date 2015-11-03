5da7192699bfef1569c61afb793d432bfad27210
Miguel Fonseca <miguelcsf@gmail.com>
2015-11-03 02:55:44 +0000

    (WIP) Themes: Start going full-Redux client-side

--- client/state/index.js
+++ client/state/index.js
@@ -9,6 +9,7 @@ import { createStore, applyMiddleware, combineReducers } from 'redux';
  */
 import sharing from './sharing/reducer';
 import sites from './sites/reducer';
+import themes from 'lib/themes/reducers';
 import ui from './ui/reducer';
 
 /**
@@ -17,6 +18,7 @@ import ui from './ui/reducer';
 const reducer = combineReducers( {
 	sharing,
 	sites,
+	themes,
 	ui
 } );
 
